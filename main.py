06a17fbd5656c6ed6854621284e5972d046e5700
Saxda <githubrm@gmail.com>
2023-06-20 20:28:31 +0200

    changes

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import os
 import sys
 import shutil
+
+# Get the current working 
+# directory (CWD) 
+cwd = os.getcwd() 
+      
+# Print the current working 
+# directory (CWD) 
+print("Current working directory:", cwd)
